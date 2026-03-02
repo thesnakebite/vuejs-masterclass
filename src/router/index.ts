2c37688369a3d04cacc903530818ee03096d1994
Álvaro Navarro <alvaro@thesnakebite.es>
2026-03-02 08:43:15 +0100

    add 404 catch-all route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { h } from 'vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,15 @@ const router = createRouter({
             name: 'single-project',
             component: () =>
                 import('@/views/SingleProjectView.vue'),
+        },
+        {
+            path: '/:catchAll(.*)*',
+            name: 'NotFound',
+            component: h(
+                'p',
+                { style: 'color: red' },
+                '404 Not Found',
+            ),
         },
     ],
 })
